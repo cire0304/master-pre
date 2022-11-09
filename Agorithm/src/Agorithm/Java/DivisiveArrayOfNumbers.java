f44fa377caf8c18ec9921de8f6b605d904b75128
DESKTOP-R4DA79P\dltpw <dltpwns0@gmail.com>
2022-11-09 21:37:14 +0900

    나누어 떨어지는 숫자 배열

---
+++ Agorithm/src/Agorithm/Java/DivisiveArrayOfNumbers.java
@@ -0,0 +1,29 @@
+package Agorithm.Java;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+class Solution1 {
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int j : arr) {
+            if (j % divisor == 0) {
+                arrayList.add(j);
+            }
+        }
+        if(arrayList.size() == 0 ){
+            arrayList.add(-1);
+        }
+
+        arrayList.sort(Comparator.naturalOrder());
+
+        answer = new int[arrayList.size()];
+        for(int i=0;i<arrayList.size();i++){
+            answer[i] = arrayList.get(i);
+        }
+
+        return answer;
+    }
+}
