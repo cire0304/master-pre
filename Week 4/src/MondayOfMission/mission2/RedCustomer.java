9d5e30c145d8d4ca6dac66366edc663817c1725c
DESKTOP-R4DA79P\dltpw <dltpwns0@gmail.com>
2022-11-21 19:21:22 +0900

    4주차 월요일 미션 2 구현

---
+++ Week 4/src/MondayOfMission/mission2/RedCustomer.java
@@ -0,0 +1,15 @@
+package MondayOfMission.mission1;
+
+public class RedCustomer extends Customer{
+
+
+
+    public RedCustomer(String name, int Id, GRADE grade){
+        this.name = name;
+        this.id = Id;
+        this.grade = grade;
+    }
+
+
+
+}
